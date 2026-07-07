1d42c5071d09db112e43df261a0c9665e5b84397
Jesse Carroll <jessecarroll@Jesses-Mac-mini.local>
2026-07-07 16:19:08 -0500

    wip(84f31210-f650-4bf3-be9c-00cf997d3255): add-main-tsx

--- clients/web/src/main.tsx
+++ clients/web/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = document.getElementById('root');
-if (!root) throw new Error('Missing #root element');
-createRoot(root).render(<React.StrictMode><App /></React.StrictMode>);
+const container = document.getElementById('root');
+if (!container) {
+    throw new Error('Missing root element');
+}
+createRoot(container).render(<React.StrictMode><App /></React.StrictMode>);
+
